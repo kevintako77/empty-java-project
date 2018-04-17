2cb3d48e6d902daa86cec69dfc5d5237b1914448
kevintako77 <kevin_tako@hotmail.com>
2018-04-17 14:14:23 +0200

    operaciones

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,23 +6,13 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-        byte b = 65;
-        char c = 'A';
-        short s = 65;
-        int i = 65;
-        long l = 65L;
-        float f = 65f;
-        double d = 65.55;
-        boolean bol = true;
 
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(s);
-        System.out.println(i);
-        System.out.println(l);
-        System.out.println(f);
-        System.out.println(d);
-        System.out.println(bol);
+        System.out.println(2+4-3*2);
+        System.out.println(2*4*2/2);
+        System.out.println(1/2);
+        System.out.println(5*(1/2));
+        System.out.println(5f*1/2);
+
     }
 
 }
