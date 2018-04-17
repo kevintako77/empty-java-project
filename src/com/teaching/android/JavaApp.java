b50ca1b3aaecb629d072328adb7528325b618636
kevintako77 <kevin_tako@hotmail.com>
2018-04-17 13:08:49 +0200

    Mi primera huella (*_*)

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,7 +6,23 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        byte b = 65;
+        char c = 'A';
+        short s = 65;
+        int i = 65;
+        long l = 65L;
+        float f = 65f;
+        double d = 65.55;
+        boolean bol = true;
+
+        Sistem.out.println(b);
+        Sistem.out.println(c);
+        Sistem.out.println(s);
+        Sistem.out.println(i);
+        Sistem.out.println(l);
+        Sistem.out.println(f);
+        Sistem.out.println(d);
+        Sistem.out.println(bol);
     }
 
 }
